50d4895e0e0f91c3260572eff0d2f9548a71d66e
SofiaShapiro <sofia.udo@gmail.com>
2014-12-05 17:51:14 -0500

    dynamic categories graphmap

--- controllers/graphmap.js
+++ controllers/graphmap.js
@@ -11,22 +11,23 @@ $.chartWebView.addEventListener('load', function() {
  	var days = [];
  	
  	var info = args.db.execute('select * from workout_info where exercise = ?', title);
- 	var infodays = args.db.execute('select * from workouts where id = ?', info.fieldByName('workout_id'));
+ 
  	
  	Ti.API.info('DB Output:' +info.isValidRow());
- 	Ti.API.info('DB Output:' +infodays.isValidRow());
  	
  	//stops running here
- 	while(info.isValidRow() && infodays.isValidRow());
+ 	while(info.isValidRow());
  	{
- 		points.push(info.fieldByName(typestr));
- 		Ti.API.info("Point: " +info.fieldByName(typestr));
- 		
- 		days.push(infodays.fieldByName('timestamp'));
- 		Ti.API.info("Point: " +infodays.fieldByName('timestamp'));
- 		
+ 		var infodays = args.db.execute('select * from workouts where id = ?', info.fieldByName('workout_id'));
+ 		if (infodays.isValidRow())
+ 		{
+	 		points.push(info.fieldByName(typestr));
+	 		Ti.API.info("Point: " +info.fieldByName(typestr));
+	 		
+	 		days.push(infodays.fieldByName('timestamp'));
+	 		Ti.API.info("Point: " +infodays.fieldByName('timestamp'));
+ 		}
  		info.next();
- 		days.next();
  	}
  	// makes the points for the graph
  	var data = "[{name: '";
@@ -46,15 +47,15 @@ $.chartWebView.addEventListener('load', function() {
     //var cat = "['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9', 'Day 10', 'Day 11', 'Day 12', 'Day 13', 'Day 14']";
 	
 	var cat = "['";
-	 + "', '";
+	
  	for (k = 0; k < days.length; k++)
  	{
-  		cat = cat + days[0];
-  		if (k != points.length - 1)
+  		cat = cat + days[k];
+  		if (k != days.length - 1)
  	 	{
  	 		cat = cat + "', '";
  		}
- 	 }
+ 	}
  	 	cat = cat + "']";
  	 	
     $.chartWebView.evalJS('plotChart("'+title+'",'+cat+','+data+')');
