967f4e89bc34ae4ce7a62f69040124a19d505c55
timmaka <tmakalinao@college.harvard.edu>
2014-12-05 16:43:35 -0500

    Added comments

--- controllers/graphmap.js
+++ controllers/graphmap.js
@@ -19,7 +19,7 @@ $.chartWebView.addEventListener('load', function() {
  		Ti.API.info("Point: " +info.fieldByName(typestr));
  		info.next();
  	}
- 
+ 	// makes the points for the graph
  	var data = "[{name: '";
  	data = data + typestr;
  	data = data + "', data: [";
@@ -32,7 +32,7 @@ $.chartWebView.addEventListener('load', function() {
  		}
  	 }
  	 	data = data + "]}]";
-
+	// two weeks for graph
     var cat = "['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9', 'Day 10', 'Day 11', 'Day 12', 'Day 13', 'Day 14']";
 	
     $.chartWebView.evalJS('plotChart("'+title+'",'+cat+','+data+')');
