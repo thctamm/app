8980ae62b933498b77756f0c4c4dc3a10b8e5931
thctamm <tamm@college.harvard.edu>
2014-12-05 17:21:35 -0500

    sab4

--- controllers/stats.js
+++ controllers/stats.js
@@ -12,66 +12,66 @@ var mainView = Titanium.UI.createView({
     width:'100%'
 });
 
-// // event listener for swipe functionality
-// mainView.addEventListener('swipe', function(e){
-	// if (e.direction == 'right')
-	// {
-		// if (Ti.Platform.osname == "iphone" || Ti.Platform.osname == "ipad")
-		// {
-			// nav.close();
-		// }
-		// else
-		// {
-		// statsWin.close();
-		// }
-	// }
-// });
-// 
-// var total_weight = 0;
-// var total_workouts = 0;
-// 
-// var data = args.db.execute('SELECT * FROM stats');
-// 
-// if (data.isValidRow())
-// {
-	// total_weight = data.fieldByName('weight');
-	// total_workouts = data.fieldByName('workouts');
-// }
-// 
-// data.close();
-// 
-// var label1 = Titanium.UI.createLabel({
-    // text:'Total weight lifted: ' + total_weight,
-    // color: 'black',
-    // left: 10,
-    // top: 10,
-    // height: 30
-// });
-// 
-// var label2 = Titanium.UI.createLabel({
-    // text:'Total number of workouts: ' + total_workouts,
-    // color: 'black',
-    // left: 10,
-    // top: 50,
-    // height: 30
-// });
-// 
-// var most_used = args.db.execute('SELECT * FROM list where used > 0 ORDER BY used DESC LIMIT 1');
-// 
-// if (most_used.isValidRow())
-// {
-	// var label3 = Titanium.UI.createLabel({
-	    // text:'Most used exercise: ' + most_used.fieldByName('name') + ', ' + most_used.fieldByName('used') + ' times',
-	    // color: 'black',
-	    // left: 10,
-	    // top: 90,
-	    // height: 30
-	// });	
-	// mainView.add(label3);
-// }
-// 
-// mainView.add(label1);
-// mainView.add(label2);
+// event listener for swipe functionality
+mainView.addEventListener('swipe', function(e){
+	if (e.direction == 'right')
+	{
+		if (Ti.Platform.osname == "iphone" || Ti.Platform.osname == "ipad")
+		{
+			nav.close();
+		}
+		else
+		{
+		statsWin.close();
+		}
+	}
+});
+
+var total_weight = 0;
+var total_workouts = 0;
+
+var data = args.db.execute('SELECT * FROM stats');
+
+if (data.isValidRow())
+{
+	total_weight = data.fieldByName('weight');
+	total_workouts = data.fieldByName('workouts');
+}
+
+data.close();
+
+var label1 = Titanium.UI.createLabel({
+    text:'Total weight lifted: ' + total_weight,
+    color: 'black',
+    left: 10,
+    top: 10,
+    height: 30
+});
+
+var label2 = Titanium.UI.createLabel({
+    text:'Total number of workouts: ' + total_workouts,
+    color: 'black',
+    left: 10,
+    top: 50,
+    height: 30
+});
+
+var most_used = args.db.execute('SELECT * FROM list where used > 0 ORDER BY used DESC LIMIT 1');
+
+if (most_used.isValidRow())
+{
+	var label3 = Titanium.UI.createLabel({
+	    text:'Most used exercise: ' + most_used.fieldByName('name') + ', ' + most_used.fieldByName('used') + ' times',
+	    color: 'black',
+	    left: 10,
+	    top: 90,
+	    height: 30
+	});	
+	mainView.add(label3);
+}
+
+mainView.add(label1);
+mainView.add(label2);
 statsWin.add(mainView);
 if (Ti.Platform.osname == "iphone" || Ti.Platform.osname == "ipad")
 {
